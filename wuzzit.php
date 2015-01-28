961a38b8b1964b1887c8ae9166a6f9bf8fe368fa
Mats Nyberg <info@jollobajano.com>
2015-01-28 15:13:44 +0100

    rader och kolumner (mod-operatorn)

--- wuzzit.php
+++ wuzzit.php
@@ -10,20 +10,18 @@ Author URI: http://your-home-url
 
 remove_shortcode('gallery');
 add_shortcode('gallery', 'parse_gallery_shortcode');
-
-
-
-function parse_gallery_shortcode($atts) {
- 
+function parse_gallery_shortcode($atts) { 
     global $post;
- 
+
+    $num_cols = 3;
+    $md_col_class = "col-md-" . strval(12 / $num_cols);
+
     if ( ! empty( $atts['ids'] ) ) {
         // 'ids' is explicitly ordered, unless you specify otherwise.
         if ( empty( $atts['orderby'] ) )
             $atts['orderby'] = 'post__in';
         $atts['include'] = $atts['ids'];
     }
- 
     extract(shortcode_atts(array(
         'orderby' => 'menu_order ASC, ID ASC',
         'include' => '',
@@ -32,10 +30,9 @@ function parse_gallery_shortcode($atts) {
         'icontag' => 'dt',
         'captiontag' => 'dd',
         'columns' => 3,
-        'size' => 'medium',
+        'size' => 'medium', //array(72,72),
         'link' => 'file'
     ), $atts));
- 
  
     $args = array(
         'post_type' => 'attachment',
@@ -52,7 +49,7 @@ function parse_gallery_shortcode($atts) {
     }
  
     $images = get_posts($args);
-
+    $count = 0;
     echo '<div class="row"><!-- gallery -->';
     
     foreach ( $images as $image ) {    
@@ -64,13 +61,17 @@ function parse_gallery_shortcode($atts) {
         $image_alt = get_post_meta($image->ID,'_wp_attachment_image_alt', true);
  
         // render your gallery here
-        echo '<div class="col-xs-6 col-md-4">';
+        echo "<div class='col-xs-6 $md_col_class'>";
         echo '<a href="#" class="thumbnail">';
 
         echo wp_get_attachment_image($image->ID, $size);
         
         echo '</a>';
         echo '</div>';
+	$count++;
+	if( !($count % 3) ) {
+	  echo '</div><div class="row">';
+	}
     }
     echo '</div><!-- /gallery -->';
 }
